3ddfaea416270df57c73488cd366884e3e2d90ba
yamanjr10 <yaman.jreditz@gmail.com>
2026-02-03 03:23:12 +0545

    Anipulse Version 2.0.0

--- Backend/models/anime.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const AnimeSchema = new mongoose.Schema({
-  userId: String,
-  title: String,
-  status: String,
-});
-
-module.exports = mongoose.model("Anime", AnimeSchema);
