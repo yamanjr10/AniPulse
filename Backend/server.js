77de4fe89169198f5eccc5a62f109f4a68781c45
yamanjr10 <yaman.jreditz@gmail.com>
2026-02-06 21:54:24 +0545

    Anipulse Version 2.0.0

--- Backend/server.js
+++ Backend/server.js
@@ -5,11 +5,7 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 // Routes
