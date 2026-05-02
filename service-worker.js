a92ca909f6de8899c2daafe47de453a709cd6c40
yamanjr10 <yaman.jreditz@gmail.com>
2026-05-03 00:20:37 +0545

    Anipulse Version 2.0.0

--- service-worker.js
+++ service-worker.js
@@ -9,16 +9,11 @@ const STATIC_ASSETS = [
   '/dashboard.css',
   '/main.js',
   '/manifest.json',
-  // Add all your icons
+  // icons
   '/icon/icon.png',
-  '/icon/icon-72x72.png',
-  '/icon/icon-96x96.png',
-  '/icon/icon-128x128.png',
-  '/icon/icon-144x144.png',
-  '/icon/icon-152x152.png',
-  '/icon/icon-192x192.png',
-  '/icon/icon-384x384.png',
-  '/icon/icon-512x512.png'
+  '/icon/icon-16.png',
+  '/icon/icon-32.png',
+  '/icon/icon-96x96.png'
 ];
 
 // Install event
